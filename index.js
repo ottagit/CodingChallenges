de4dcd1ed8c9fc7343b241d9b8d0540581ff6486
Chris Otta <ottagit@users.noreply.github.com>
2022-01-11 05:25:38 +0000

    map twoDsort and not finSort
    Delimit on empty string ('') and not comma (',')

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-var addTwoNumbers = function(l1, l2) {
+let addTwoNumbers = function(l1, l2) {
     let twoD = new Array(l1, l2);
     let twoDsort = twoD.sort((l1, l2) => l1.length - l2.length);
-    let finSort;
-    console.log(twoDsort);
+    
     if (twoDsort[0].length != twoDsort[1].length) {
         while (twoDsort[0].length != twoDsort[1].length) {
             twoDsort[0].push(0);
         }
-        finSort = twoDsort[0].reverse();
+        twoDsort[0].reverse();
     }
-    console.log(finSort);
-   
-    let sum = finSort.map((e, i) => Number(e.join(','))).
+    
+    let sum = twoDsort.map(e => Number(e.join(''))).
     reduce((a, c) => a + c);
-    console.log(sum);
 
     return Array.from(sum + '').
     map(e => Number(e)).reverse();
-
-    //console.log(twoSum);
 };
 
-addTwoNumbers([9,9,9,9,9,9,9], [9,9,9,9]);
+addTwoNumbers([2,4,3], [5,6,4]);
